327c9191efced875ef36dbb0fb3e5bb3b357c777
Hanzala Khan <106017090+Hanzala-khan1@users.noreply.github.com>
2024-04-20 04:11:38 +0500

    pull request merge

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use(formidable());
-app.options('*', cors());
+// app.options('*', cors());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
